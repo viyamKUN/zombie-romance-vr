c810eb2db07733d9409ae273b807b46c4192966c
Suya <77002255+suyaneeko@users.noreply.github.com>
2021-05-21 03:51:36 +0900

    VR Items Socket(Inventory)
    VR - Items Socket(use as inventory that player keeps)

---
+++ Assets/Scripts/VR/FollowItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowItem : MonoBehaviour
+{
+    public Transform target;    //camera
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position + Vector3.up * offset.y + 
+            Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x +
+            Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z;
+    }
+}
